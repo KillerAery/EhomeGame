67aef26005cd0eed4b58dfc6348d7a201f756f77
AMzzcc <793048004@qq.com>
2019-01-26 17:39:02 +0800

    缓存

---
+++ GGJ/Assets/Scripts/Wardrobe.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wardrobe : MonoBehaviour
+{
+    public bool isOpened=false;
+    public GameObject enemy;
+    public GameObject woodBox;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       
+    }
+
+    public void Open()
+    {
+        //Instantiate(enemy, transform.position, Quaternion.identity);
+        woodBox.SendMessage("toOpen");
+        isOpened = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!isOpened&&collision.collider.tag=="Player")
+        {
+            Open();
+        }
+    }
+
+    public void toOpen(string str)
+    {
+
+        tex.SetActive(true);
+        ima.SetActive(true);
+        inp.SetActive(true);
+        text.text = "你发现了一个木箱";
+        if (str == password)
+        {
+            Debug.Log("right");
+
+        }
+
+    }
+}
---
+++ GGJ/Assets/Scripts/WoodBox.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WoodBox : MonoBehaviour
+{
+    public GameObject tex;
+    public GameObject ima;
+    public GameObject inp;
+    public Text text;
+    public Image image;
+    public InputField inputField;
+    public string password="0606";
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void toOpen(string str)
+    {
+
+        tex.SetActive(true);
+        ima.SetActive(true);
+        inp.SetActive(true);
+        text.text = "你发现了一个木箱";
+        if (str==password)
+        {
+            Debug.Log("right");
+            
+        }
+        
+    }
+}
