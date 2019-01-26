ca76ef2921cdee72ff37f86c9da34c0060937590
AMzzcc <793048004@qq.com>
2019-01-26 09:59:51 +0800

    最基本版本

---
+++ GGJ/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float health = 3;    //生命值
+    public float maxhealth = 3; //最大生命值
+    private int dr = 1;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * x * moveSpeed * Time.deltaTime, Space.World);
+        if (Mathf.Abs(x) >= 0.01f)
+        {
+            dr = (int)Mathf.Sign(x);
+            transform.localScale = new Vector2(dr * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+            
+        }
+
+        float y = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * y * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
---
+++ GGJ/Assets/Scripts/camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    public float Max;
+    public float Min;
+    private Vector2 offset;     //相机与人物偏移量
+    private Vector2 ve;
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        if (transform.position.x >= Min && transform.position.x <= Max)
+        {
+            Vector3 s = Vector2.SmoothDamp(transform.position, target.position, ref ve, 0.5f);
+            s.z = transform.position.z;
+            transform.position = s;
+        }
+    }
+}
