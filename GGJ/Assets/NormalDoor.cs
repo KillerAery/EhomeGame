1e29cd6ff3ab96afa56eaffa5d854d9b6cf8cd22
Aery <KillerAery@foxmail.com>
2019-01-27 01:59:16 +0800

    test5

---
+++ GGJ/Assets/NormalDoor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalDoor : MonoBehaviour
+{
+    public Sprite openSprite;
+    public Sprite closeSprite;
+    public bool opening = false;
+
+    private SpriteRenderer spriteRenderer;
+    public new BoxCollider2D collider2D;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!opening)
+            {
+                opening = true;
+                spriteRenderer.sprite = openSprite;
+                collider2D.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            if (opening)
+            {
+                opening = false;
+                spriteRenderer.sprite = closeSprite;
+                collider2D.enabled = true;
+            }
+        }
+    }
+}
---
+++ GGJ/Assets/StudyDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudyDoor : MonoBehaviour
+{
+    public GameObject tip;
+    public Sprite openSprite;
+    private TextManager textManager;
+    private SpriteRenderer spriteRenderer;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        textManager = GameObject.FindGameObjectWithTag("TextManager").GetComponent<TextManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            tip.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                var player = collision.gameObject.GetComponent<Player>();
+                if (player.studykey == true)
+                {
+                    textManager.ShowText("看来很顺利开了书房的锁。");
+                    spriteRenderer.sprite = openSprite;
+                }
+                else
+                {
+                    textManager.ShowText("书房门锁上了，我开不了。");
+                }
+            }
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        tip.SetActive(false);
+    }
+}
