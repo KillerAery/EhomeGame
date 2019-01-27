f055bc176291c6e30c7f231a5889bb89ff20a03f
AMzzcc <793048004@qq.com>
2019-01-27 13:10:38 +0800

    保险柜

--- GGJ/Assets/StrongBox.cs
+++ GGJ/Assets/StrongBox.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class StrongBox : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string password = "2333";
+    public GameObject inp;
+    public InputField inputField;
+    public Player player;
+    private TextManager textManager;
+    public GameObject tip;
+    public bool empty = false;
+
     void Start()
     {
         
@@ -14,5 +21,36 @@ public class StrongBox : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            tip.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (empty)
+                {
+                    textManager.ShowText("这什么都没有。");
+                    return;
+                }
+                else
+                {
+                    inp.SetActive(true);
+                }
+            }
+            tip.SetActive(false);
+            inp.SetActive(false);
+        }
+    }
+
+    public void toOpen()
+    { //
+        if (inputField.text == password)
+        {
+            textManager.ShowText("一把钥匙");
+            player.gatekey = true;
+        }
     }
 }
